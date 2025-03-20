53c7afeaa05969ea714d65c872527ee4fe1a9c78
Sumeet <rane.su@northeastern.edu>
2025-03-20 02:13:19 -0400

    Added S3 and RDS creation code with Iam Roles

--- variables.tf
+++ variables.tf
@@ -37,3 +37,56 @@ variable "key_pair_name" {
   description = "The name of the AWS Key Pair"
   type        = string
 }
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true # Marks this as sensitive to avoid logging
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_identifier" {
+  description = "RDS instance identifier"
+  type        = string
+}
+
+variable "db_engine" {
+  description = "Database engine type"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "Instance type for the database"
+  type        = string
+}
+
+variable "db_allocated_storage" {
+  description = "Allocated storage size (in GB) for the database"
+  type        = number
+}
+
+# -------
+
+# variable "aws_access_key" {
+#   description = "AWS ACCESS KEY"
+#   type        = string
+# }
+
+# variable "aws_secret_access_key" {
+#   description = "AWS SECRET KEY"
+#   type        = string
+# }
+
+# variable "s3_bucket_name" {
+#   description = "S3 Bucket Name"
+#   type        = string
+# }
